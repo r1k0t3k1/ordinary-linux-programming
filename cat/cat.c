4ecef4f5d7d1f1914fa29220582e881918e76435
riko-teki <boiledshrimps0109@gmail.com>
2022-05-19 14:22:06 +0900

    add 5.8-practice-1

--- cat/cat.c
+++ cat/cat.c
@@ -5,45 +5,38 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-static void do_cat(const char *path);
+static void do_cat(FILE *f);
 static void die(const char *s);
 
 int main(int argc, char *argv[]){
   int i;
 
-  if (argc < 2) {
-    fprintf(stderr, "%s: file name are not given\n", argv[0]);
-    exit(1);
+  if (argc == 1) {
+    do_cat(stdin);
+    exit(0);
   }
 
+  FILE *f;
   for (i = 1; i < argc; i++){
-    do_cat(argv[i]);
+    f = fopen(argv[i], "r");
+    if (!f) die(argv[i]);
+    do_cat(f);
   }
+  fclose(f);
   exit(0);
 }
 
 #define BUFFER_SIZE 2048
 
-static void do_cat(const char *path) {
-  int fd;
-  unsigned char buf[BUFFER_SIZE];
+static void do_cat(FILE *f) {
   int n;
 
-  fd = open(path, O_RDONLY);
-  if (fd < 0) die(path);
-  
-  while(1) {
-    n = read(fd, buf, sizeof buf); 
-    if (n < 0) die(path);
-    if (n == 0) break;
-    
-    if (write(STDOUT_FILENO, buf, n) < 0) die(path); 
+  while((n = fgetc(f)) != EOF) {
+    if (fputc(n, stdout) < 0) die("fputc");
   }
-  if (close(fd) < 0) die(path);
 }
 
 static void die(const char *s) {
   perror(s);
   exit(1);
 }
-
